e8486858ecd4df75e29ee125a5c0559fcc31df96
JuanVal <elxatonet@gmail.com>
2022-10-23 11:26:18 +0200

    Añadiendo boton next

--- src/index.svelte
+++ src/index.svelte
@@ -1,13 +1,28 @@
 <svelte:options tag="njv-pagination" />
 
 <script>
-  import avatars from "./avatares.json";
+  import avatares from "./avatares.json";
+  let primeraPagina = 1;
+  let maxAvatarsPerPage = 4;
+  const totalPages = (payload) => Math.ceil(payload.length / maxAvatarsPerPage);
+
+  console.log(avatares.data.slice(primeraPagina - 1, maxAvatarsPerPage));
+  const nextPage = () => {
+    primeraPagina += primeraPagina;
+    maxAvatarsPerPage = maxAvatarsPerPage * 2;
+    console.log(totalPages(avatares.data));
+
+    console.log(primeraPagina, maxAvatarsPerPage);
+  };
 </script>
 
 <h2>Pagination</h2>
-{#each avatars as avatar}
-  <img src={avatar.url} alt="Imagen avatar" />
-{/each}
+{#if avatares.status === 200}
+  {#each avatares.data as avatar}
+    <img src={avatar.url} alt="Imagen avatar" width="32" />
+  {/each}
+{/if}
+<button on:click={nextPage}>Next</button>
 
 <style>
 </style>
