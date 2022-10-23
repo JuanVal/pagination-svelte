170d7cdcca87887bc2fc9b98cedaf4603bb7a9c9
JuanVal <elxatonet@gmail.com>
2022-10-23 10:59:04 +0200

    Creando el archivo avatars.json

--- src/index.svelte
+++ src/index.svelte
@@ -1,9 +1,13 @@
 <svelte:options tag="njv-pagination" />
 
 <script>
-
+  import avatars from "./avatares.json";
 </script>
-<h2>Pagination</h2>
-<style>
 
+<h2>Pagination</h2>
+{#each avatars as avatar}
+  <img src={avatar.url} alt="Imagen avatar" />
+{/each}
+
+<style>
 </style>
